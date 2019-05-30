c098a08930a6aab5ced98f75986ea829dacbd4be
Serega <planshetsph@gmail.com>
2019-05-30 11:21:51 -0700

    set up parameters

--- src/test/java/core/Suite1.java
+++ src/test/java/core/Suite1.java
@@ -1,9 +1,17 @@
 package core;
 
 import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class Suite1 {
+	
+	@Parameters("environment")
+	@BeforeClass
+	public void setup(String environment) {
+		System.out.println("Environment is " + environment);
+	}
 
 	@Test
 	public void test1() {
